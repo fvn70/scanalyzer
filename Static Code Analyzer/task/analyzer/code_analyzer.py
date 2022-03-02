6d3eb53634384227253bdade15bf3fb76cb6f11f
fvn70 <fvn_reg@mail.ru>
2022-03-02 13:37:29 +0700

    Stage 3/5: Analyze multi-file projects

--- Static Code Analyzer/task/analyzer/code_analyzer.py
+++ Static Code Analyzer/task/analyzer/code_analyzer.py
@@ -1,50 +1,64 @@
+import os.path
 import re
+import sys
 
 
-def s001(num, line):
+def s001(fn, num, line):
     if len(line) > 79:
-        print(f'Line {num}: S001 Too long')
+        print(f'{fn}: Line {num}: S001 Too long')
 
 
-def s002(num, line):
+def s002(fn, num, line):
     if (len(line) - len(line.lstrip())) % 4 != 0:
-        print(f'Line {num}: S002 Indentation is not a multiple of four')
+        print(f'{fn}: Line {num}: S002 Indentation is not a multiple of four')
 
 
-def s003(num, line):
+def s003(fn, num, line):
     if ';' in line:
         exp = '[\'\"].*;.*[\'\"]'
         is_str = re.search(exp, line)
         if not is_str and ('#' not in line or line.index(';') < line.index('#')):
-            print(f'Line {num}: S003 Unnecessary semicolon after a statement')
+            print(f'{fn}: Line {num}: S003 Unnecessary semicolon after a statement')
 
 
-def s004(num, line):
+def s004(fn, num, line):
     if '#' in line:
         k = line.index('#')
         if k > 2 and line[k - 2: k] != '  ':
-            print(f'Line {num}: S004 Less than two spaces before inline comments')
+            print(f'{fn}: Line {num}: S004 Less than two spaces before inline comments')
 
 
-def s005(num, line):
+def s005(fn, num, line):
     if '#' in line and 'todo' in line.lower():
-        print(f'Line {num}: S005 TODO found')
+        print(f'{fn}: Line {num}: S005 TODO found')
 
 
-def s006(num, line):
+def s006(fn, num, line):
     if cnt_blank > 2:
-        print(f'Line {num}: S006 More than two blank lines')
+        print(f'{fn}: Line {num}: S006 More than two blank lines')
 
 
-# fn = 'tst.py'
-fn = input()
-with open(fn, 'r') as file:
-    cnt_blank = 0
-    for i, line in enumerate(file):
-        if line.strip():
-            for n in range(6):
-                s = f's00{n + 1}({i + 1}, line)'
-                eval(s)
-            cnt_blank = 0
-        else:
-            cnt_blank += 1
+def chk_file(fn):
+    with open(fn, 'r') as file:
+        global cnt_blank
+        for i, line in enumerate(file):
+            if line.strip():
+                for n in range(6):
+                    s = f's00{n + 1}(fn, {i + 1}, line)'
+                    eval(s)
+                cnt_blank = 0
+            else:
+                cnt_blank += 1
+
+
+args = sys.argv
+path = args[1]
+is_file = os.path.isfile(path)
+base = os.path.dirname(path) if is_file else path
+files = [os.path.split(path)[1]] if is_file else os.listdir(base)
+
+cnt_blank = 0
+
+for file in files:
+    fn = base + os.sep + file
+    chk_file(fn)
